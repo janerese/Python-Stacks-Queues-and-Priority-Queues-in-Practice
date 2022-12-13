7e6123e910e3de73841a8c7541f1139df50dd149
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 21:37:40 +0800

    Defining asyncronous worker performing the job

--- async_queues.py
+++ async_queues.py
@@ -30,6 +30,23 @@ async def main(args):
     finally:
         await session.close()
 
+async def worker(worker_id, session, queue, links, max_depth):
+    print(f"[{worker_id} starting]", file=sys.stderr)
+    while True:
+        url, depth = await queue.get()
+        links[url] +=1
+        try:
+            if depth <= max_depth:
+                print(f"[{worker_id} {depth=} {url=}]", file=sys.stderr)
+                if html := await fetch_html(session, url):
+                    for link_url in parse_links(url, html):
+                        await queue.put(Job(link_url, depth + 1))
+        except aiohttp.ClientError:
+            print(f"[{worker_id} failed at {url=}]", file=sys.stderr)
+
+        finally:
+            queue.task_done()
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("url")
