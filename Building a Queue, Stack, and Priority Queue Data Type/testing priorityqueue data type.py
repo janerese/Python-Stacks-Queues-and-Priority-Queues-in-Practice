9a049fadee99ab76e43572c2fd31a4dfb00abb71
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 12:34:12 +0800

    Testing PriorityQueue Data Type

---
+++ Building a Queue, Stack, and Priority Queue Data Type/testing priorityqueue data type.py
@@ -0,0 +1,16 @@
+# Testing PriorityQueue Data Type
+
+from queues import PriorityQueue
+
+# Priority Levels
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wiper turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal pressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue())
