08bcf1a87bb9554e79a0bad660ccd39d97b68ddd
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 17:21:51 +0800

    Importing argparse and queue module

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -1,2 +1,10 @@
 # Using Python's thread-safe queues
 
+import argparse
+from queue import LifoQueue, PriorityQueue, Queue
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": LifoQueue,
+    "heap": PriorityQueue
+}
