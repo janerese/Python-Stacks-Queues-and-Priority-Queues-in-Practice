f477e71c6346157d1b48815792d96c6ef7fd4381
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 23:17:50 +0800

    Updated the main() function

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -6,6 +6,7 @@ from hashlib import md5
 from string import ascii_lowercase
 import multiprocessing
 from dataclasses import dataclass
+import argparse
 
 # To minimize the cost of data serialization between your processes, each worker will produce its own chunk of letter combinations based on the range of indices specified in a dequeued job object
 class Combinations:
@@ -63,11 +64,22 @@ def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
             if hashed == hash_value:
                 return text_bytes.decode("utf-8")
 
-# Call the function with a sample MD5 hash value passed as an argument and measure its execution time using a Python timer
-def main():
-    t1 = time.perf_counter()
-    text = reverse_md5("a9d1cbf71942327e98b40cf5ef38a960")
-    print(f"{text} (found in {time.perf_counter() - t1:.1f}s)")
+def main(args):
+    queue_in = multiprocessing.Queue()
+    queue_out = multiprocessing.Queue()
+
+    workers = [
+        Worker(queue_in, queue_out, args.hash_value)
+        for _ in range(args.num_workers)
+    ]
+
+    for worker in workers:
+        worker.start()
+
+    for text_length in range(1, args.max_length + 1):
+        combinations = Combinations(ascii_lowercase, text_length)
+        for indices in chunk_indices(len(combinations), len(workers)):
+            queue_in.put(Job(combinations, *indices))
 
 # Distributing Workload Evenly in Chunks
 # Calculate indices of the subsequent chunks
