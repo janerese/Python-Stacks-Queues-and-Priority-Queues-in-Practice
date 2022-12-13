a46f1e88fea49435c9382ab7609d5bcade1280cb
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 18:16:08 +0800

    Finished View class

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -76,11 +76,50 @@ class Worker(threading.Thread):
             sleep(delay / 100)
             self.progress += 1
 
+# The View class that defines a view that renders the current state of your producers, consumers, and the queue ten times a second
 class View:
     def __init__(self, buffer, producers, consumers):
         self.buffer = buffer
         self.producers = producers
         self.consumers = consumers
+    
+    def animate(self):
+        with Live(self.render(), screen=True, refresh_per_second=10) as live:
+            while True:
+                live.update(self.render())
+
+    def render(self):
+        match self.buffer:
+            case PriorityQueue():
+                title = "Priority Queue"
+                products = map(str, reversed(list(self.buffer.queue)))
+            case LifoQueue():
+                title = "Stack"
+                products = list(self.buffer.queue)
+            case Queue():
+                title = "Queue"
+                products = reversed(list(self.buffer.queue))
+            case _:
+                title = products = ""
+
+        rows = [
+            Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)
+        ]
+        pairs = zip_longest(self.producers, self.consumers)
+        for i, (producer, consumer) in enumerate(pairs, 1):
+            left_panel = self.panel(producer, f"Producer {i}")
+            right_panel = self.panel(consumer, f"Consumer {i}")
+            rows.append(Columns([left_panel, right_panel], width=40))
+        return Group(*rows)
+
+    def panel(self, worker, title):
+        if worker is None:
+            return ""
+        padding = " " * int(29 / 100 * worker.progress)
+        align = Align(
+            padding + worker.state, align="left", vertical="middle"
+        )
+        return Panel(align, height=5, title=title)
 
 # The main() function is the entry point, which receives the parsed arguments supplied by parse_args()
 def main(args):
