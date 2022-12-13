28c37f0183c8d8d2740a383866c6e96556e25ae0
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 20:53:20 +0800

    Creating new file for Using Asynchronous Queues

---
+++ Building a Queue, Stack, and Priority Queue Data Type/async_queues.py
@@ -0,0 +1,7 @@
+# Using Asynchronous Queues
+# Writing a rudimentary web crawler which recursively follows links on a specified website up to a given depth level and counts the number of visits per link
+# To fetch data asynchronously, the popular aiohttp library will be used
+# To parse HTML hyperlinks, beautifulsoup4 will be used
+# To install both libraries, type this command:
+# $ python -m pip install aiohttp beautifulsoup4
+
