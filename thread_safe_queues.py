769b131400b67c0a1887c94eb56a25552091d43d
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 17:31:07 +0800

    Defining the PRODUCTS

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -1,17 +1,40 @@
 # Using Python's thread-safe queues
 
+# Necessary modules
 import argparse
 from queue import LifoQueue, PriorityQueue, Queue
 
+# Dictionary to map queue names to their respective classes
 QUEUE_TYPES = {
     "fifo": Queue,
     "lifo": LifoQueue,
     "heap": PriorityQueue
 }
 
+# Defining the products that producers will pick at random and pretend to be working on
+PRODUCTS = (
+    ":balloon:",
+    ":cookie:",
+    ":crystal_ball:",
+    ":diving_mask:",
+    ":flashlight:",
+    ":gem:",
+    ":gift:",
+    ":kite:",
+    ":party_popper:",
+    ":postal_horn:",
+    ":ribbon:",
+    ":rocket:",
+    ":teddy_bear:",
+    ":thread:",
+    ":yo-yo:",
+)
+
+# The main() function is the entry point, which receives the parsed arguments supplied by parse_args()
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
 
+# The parse_args() supplies parsed arguments to main() function
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
