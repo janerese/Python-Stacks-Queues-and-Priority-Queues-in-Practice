45da52f2a305a835d4cab82a84910bd2f39f80b4
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 22:38:34 +0800

    Importing necessary modules

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -1,1 +1,7 @@
 # Using multiprocessing.Queue for Interprocess Communication (IPC)
+
+# Necessary modules
+import time
+from hashlib import md5
+from itertools import product
+from string import ascii_lowercase
