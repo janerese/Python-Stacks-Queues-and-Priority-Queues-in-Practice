3221116c746bdae5684249a5debba006a0d72ea7
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-13 22:48:55 +0800

    Created reverse_md5() and main() function

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -5,3 +5,21 @@ import time
 from hashlib import md5
 from itertools import product
 from string import ascii_lowercase
+
+# Defines a function that’ll try to reverse an MD5 hash value provided as the first argument
+def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
+    for length in range(1, max_length + 1):
+        for combination in product(alphabet, repeat=length):
+            text_bytes = "".join(combination).encode("utf-8")
+            hashed = md5(text_bytes).hexdigest()
+            if hashed == hash_value:
+                return text_bytes.decode("utf-8")
+
+# Call the function with a sample MD5 hash value passed as an argument and measure its execution time using a Python timer
+def main():
+    t1 = time.perf_counter()
+    text = reverse_md5("a9d1cbf71942327e98b40cf5ef38a960")
+    print(f"{text} (found in {time.perf_counter() - t1:.1f}s)")
+
+if __name__ == "__main__":
+    main()
