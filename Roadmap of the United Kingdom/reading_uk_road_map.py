b2acd91dff93fa3f56bae06f65f1c50d33b10b77
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-14 22:38:41 +0800

    Adding descriptions

--- Roadmap of the United Kingdom/reading_uk_road_map.py
+++ Roadmap of the United Kingdom/reading_uk_road_map.py
@@ -13,20 +13,26 @@ from graph import (
     dijkstra_shortest_path
 )
 
+# Testing the class City and from_dict() and load_graph() function
+print("First Testing: Using networkx module:")
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 print(nodes["london"])
+print("\n")
 print(graph)
 
 # Identifying immediate neighbors of a given city to find the available routes to follow when looking for the shortest path between two cities
+print("\n2nd testing: Identify immediate neighbors")
 for neighbor in graph.neighbors(nodes["london"]):
     print(neighbor.name)
 
 # Include the weights of the connecting edges, such as distances or the estimated travel times
+print("\n3rd testing: Neighbors with possible weights of the connecting edges")
 for neighbor, weights in graph[nodes["london"]].items():
     print(weights["distance"], neighbor.name)
 
 # The neighbors are always listed in the same order in which you defined them in the DOT file. To sort them by one or more weights, you can use the following code snippet:
+print("\n4th testing: Sorting the neighbors")
 def sort_by(neighbors, strategy):
     return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
 
@@ -36,7 +42,9 @@ def by_distance(weights):
 for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
     print(f"{weights['distance']:>3} miles, {neighbor.name}")
 
+# Breadth-First Search Using a FIFO Queue
 # Calling the nx.bfs_tree() function on your graph to reveal the breadth-first order of traversal
+print("\n5th testing: Find any place in the United Kingdom that has been granted city status in the twentieth century, starting your search in Edinburgh")
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
@@ -50,6 +58,7 @@ else:
     print("Not found")
 
 # To ensure consistent resutls, sort the neighbors according to some criteria
+print("6th testing: Visit cities with a higher latitude first")
 def order(neighbors):
     def by_latitude(city):
         return city.latitude
@@ -64,19 +73,20 @@ else:
     print("Not found")
 
 # Testing the breadth-first search and traversal implementations
+print("\n7th testing: Breadth-first search and traversal implementation")
 def is_twentieth_century(city):
     return city.year and 1901 <= city.year <= 2000
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 city = bfs(graph, nodes["edinburgh"], is_twentieth_century)
-city.name
-
-
+print(city.name)
+print("\n")
 for city in breadth_first_traverse(graph, nodes["edinburgh"]):
     print(city.name)
 
 # Shortest Path Using Breadth-First Traversal
 # Revealing the shortest path between two cities
+print("\n8th testing: Using networkx to reveal all the shortest paths between two cities")
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 city1 = nodes["aberdeen"]
@@ -85,11 +95,10 @@ city2 = nodes["perth"]
 for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
     print(f"{i}.", " → ".join(city.name for city in path))
 
-
 # Queue-based implementation of the shortest path
 # When you call the queue-based implementation of the shortest path, you get the same results as with networkx
+print("\n9th testing: Queue-based implementation of the shortest path")
 print(" → ".join(city.name for city in shortest_path(graph, city1, city2)))
-
 
 def by_latitude(city):
     return -city.latitude
@@ -100,11 +109,13 @@ print(" → ".join(
 ))
 
 # Testing the breadth-first travelsal implementation of whther two nodes remain connected or not
+print("\n10th testing: Using breadth-first traversal to check whether two nodes remain connected or not")
 print(connected(graph, nodes["belfast"], nodes["glasgow"]))
 
 print(connected(graph, nodes["belfast"], nodes["derry"]))
 
-# Depth-First Search using a LIFO Queue
+# Replacing nx.bfs_tree() with nx.dfs_tree() from earling example to compare the difference
+print("\n11th testing: Difference when nx.bfs_tree() is replaced with nx.dfs_tree() ")
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
@@ -118,19 +129,19 @@ else:
     print("Not found")
 
 # Testing the breadth_first_search() and depth_first_search() functions call search() with the corresponding traversal strategy
+print("\n12th testing: Using refactored breadth_first_search() and new depth_first_search()")
 def is_twentieth_century(city):
     return city.year and 1901 <= city.year <= 2000
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 city = dfs(graph, nodes["edinburgh"], is_twentieth_century)
-city.name
-
-
+print(city.name)
+print("\n")
 for city in depth_first_traverse(graph, nodes["edinburgh"]):
     print(city.name)
 
 # Testing  Dijkstra’s algorithm
-print("\nDijkstra’s algorithm")
+print("\n13th testing: Dijkstra’s algorithm:")
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 city1 = nodes["london"]
 city2 = nodes["edinburgh"]
@@ -142,7 +153,7 @@ for city in dijkstra_shortest_path(graph, city1, city2, distance):
     print(city.name)
 
 # Networkx implementation
-print("\nNetworkx implementation")
+print("\nCompare to Networkx implementation:")
 def weight(node1, node2, weights):
     return distance(weights)
 
