5ebdecf3380ba95e414237f4175254865970a9fb
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-14 15:20:35 +0800

    Sort the neighbors by one or more weights

--- Roadmap of the United Kingdom/reading_uk_road_map.py
+++ Roadmap of the United Kingdom/reading_uk_road_map.py
@@ -14,3 +14,15 @@ for neighbor in graph.neighbors(nodes["london"]):
 # Include the weights of the connecting edges, such as distances or the estimated travel times
 for neighbor, weights in graph[nodes["london"]].items():
     print(weights["distance"], neighbor.name)
+
+# The neighbors are always listed in the same order in which you defined them in the DOT file. To sort them by one or more weights, you can use the following code snippet:
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(weights):
+    return float(weights["distance"])
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
+
+    
