cc41bd4b307bbb76d57fed8831055c2585724bfd
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-14 13:50:08 +0800

    Created load_graph() function

--- Roadmap of the United Kingdom/graph.py
+++ Roadmap of the United Kingdom/graph.py
@@ -1,6 +1,8 @@
 # Define a custom data type representing a city in the road map
 
+# Necessary modues
 from typing import NamedTuple
+import networkx as nx
 
 # Extend a named tuple to ensure that node objects are hashable, which is required by networkx
 class City(NamedTuple):
@@ -20,3 +22,15 @@ class City(NamedTuple):
             latitude=float(attrs["latitude"]),
             longitude=float(attrs["longitude"]),
         )
+
+# To take advantage of the new class, create a new graph instance and take note of the mapping of node identifiers to city instances
+def load_graph(filename, node_factory):
+    graph = nx.nx_agraph.read_dot(filename)
+    nodes = {
+        name: node_factory(attributes)
+        for name, attributes in graph.nodes(data=True)
+    }
+    return nodes, nx.Graph(
+        (nodes[name1], nodes[name2], weights)
+        for name1, name2, weights in graph.edges(data=True)
+    )
