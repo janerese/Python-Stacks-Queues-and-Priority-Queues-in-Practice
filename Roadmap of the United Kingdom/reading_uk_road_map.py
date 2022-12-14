1a8f26ed0ea473b86a24596ad8cd0f616e14e1b6
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-14 15:11:01 +0800

    Identifying the immediate neighbors

--- Roadmap of the United Kingdom/reading_uk_road_map.py
+++ Roadmap of the United Kingdom/reading_uk_road_map.py
@@ -7,3 +7,6 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 print(nodes["london"])
 print(graph)
 
+# Identifying immediate neighbors of a given city to find the available routes to follow when looking for the shortest path between two cities
+for neighbor in graph.neighbors(nodes["london"]):
+    print(neighbor.name)
