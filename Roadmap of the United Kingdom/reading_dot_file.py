cb3bfe9bf0aa42554b397516386f3397b9491679
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-14 13:17:40 +0800

    Reading London Node

--- Roadmap of the United Kingdom/reading_dot_file.py
+++ Roadmap of the United Kingdom/reading_dot_file.py
@@ -1,5 +1,10 @@
 # Using pygraphviz to read the sample DOt file (roadmap.dot)
 import networkx as nx
+from graph import *
 print(nx.nx_agraph.read_dot("roadmap.dot"))
+# This should be printed: MultiGraph named 'Cities in the United Kingdom' with 70 nodes and 137 edges
 
-# This is printed: MultiGraph named 'Cities in the United Kingdom' with 70 nodes and 137 edges
+# networkx represents graph nodes using textual identifiers that can optionally have an associated dictionary of attributes
+graph = nx.nx_agraph.read_dot("roadmap.dot")
+print(graph.nodes["london"])
+# This should be printed: {'country': 'England', 'latitude': '51.507222', 'longitude': '-0.1275', 'pos': '80,21!', 'xlabel': 'City of London', 'year': '0'}
