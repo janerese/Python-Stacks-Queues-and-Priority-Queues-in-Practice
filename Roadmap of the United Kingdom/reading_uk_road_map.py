e2afb1c323df79eb4c45651590a590c5dc3e8c4c
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-14 15:15:22 +0800

    Include the weights of the connecting edges

--- Roadmap of the United Kingdom/reading_uk_road_map.py
+++ Roadmap of the United Kingdom/reading_uk_road_map.py
@@ -10,3 +10,7 @@ print(graph)
 # Identifying immediate neighbors of a given city to find the available routes to follow when looking for the shortest path between two cities
 for neighbor in graph.neighbors(nodes["london"]):
     print(neighbor.name)
+
+# Include the weights of the connecting edges, such as distances or the estimated travel times
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name)
