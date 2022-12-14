ebcf7bedcb1aa9c35f4d8549c0674029f508fd19
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-12-14 16:31:36 +0800

    Created function shortest_path()

--- Roadmap of the United Kingdom/graph.py
+++ Roadmap of the United Kingdom/graph.py
@@ -37,20 +37,21 @@ def load_graph(filename, node_factory):
     )
 
 # Breadth-First Search Using a FIFO Queue
-def breadth_first_traverse(graph, source):
-    queue = Queue(source)
-    visited = {source}
-    while queue:
-        yield (node := queue.dequeue())
-        for neighbor in graph.neighbors(node):
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.enqueue(neighbor)
+# The functions does not allow sorting the neighbors in a particular order
+# def breadth_first_traverse(graph, source):
+#     queue = Queue(source)
+#     visited = {source}
+#     while queue:
+#         yield (node := queue.dequeue())
+#         for neighbor in graph.neighbors(node):
+#             if neighbor not in visited:
+#                 visited.add(neighbor)
+#                 queue.enqueue(neighbor)
 
-def breadth_first_search(graph, source, predicate):
-    for node in breadth_first_traverse(graph, source):
-        if predicate(node):
-            return node
+# def breadth_first_search(graph, source, predicate):
+#     for node in breadth_first_traverse(graph, source):
+#         if predicate(node):
+#             return node
 
 # One possible solution for allowing sorting the neighbors in a particular order
 def breadth_first_traverse(graph, source, order_by=None):
@@ -69,4 +70,24 @@ def breadth_first_traverse(graph, source, order_by=None):
 def breadth_first_search(graph, source, predicate, order_by=None):
     for node in breadth_first_traverse(graph, source, order_by):
         if predicate(node):
-            return node   
+            return node
+
+# This new function takes another node as an argument and optionally lets you order the neighbors using a custom strategy
+# It also defines an empty dictionary, which you populate when visiting a neighbor by associating it with the previous node on your path
+# All key-value pairs in this dictionary are immediate neighbors without any nodes between them
+def shortest_path(graph, source, destination, order_by=None):
+    queue = Queue(source)
+    visited = {source}
+    previous = {}
+    while queue:
+        node = queue.dequeue()
+        neighbors = list(graph.neighbors(node))
+        if order_by:
+            neighbors.sort(key=order_by)
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+                previous[neighbor] = node
+                if neighbor == destination:
+                    return retrace(previous, source, destination)
